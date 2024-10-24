140b8af1a22b768055f518bba8b84d1eecad125a
adintm <adintm@think-match.com>
2024-10-24 14:47:31 +0700

    update

--- routes/home_tab_routes.js
+++ routes/home_tab_routes.js
@@ -3,5 +3,6 @@ const { checkToken } = require('../helpers/redisTransaction');
 
 module.exports = (app) => {
     app.get('/api/operator/get-home-summary/:lkf_id',(res,req) => home.handleHomeSummary(res,req))
+    app.get('/api/operator/get-home-summary-tab/:lkf_id',(res,req) => home.handleHomeTab(res,req))
     app.get('/api/operator/get-home-table/:lkf_id',(res,req) => home.handleHomeTable(res,req))
 }
