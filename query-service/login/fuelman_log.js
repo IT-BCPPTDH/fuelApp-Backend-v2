f6820ada1404c02cf196ee5b798e6bcf374cdf8d
annisaYz <annisa@think-match.com>
2024-12-24 14:28:01 +0700

    update perubahan

---
+++ query-service/login/fuelman_log.js
@@ -0,0 +1,43 @@
+const db = require('../../database/helper');
+const { QUERY_STRING } = require('../../helpers/queryEnumHelper');
+const { loginUser, signOut, fetchFromMasterStation } = require('../../helpers/httpHelper')
+const { formatYYYYMMDD } = require('../../helpers/dateHelper')
+
+const insertLog = async(data) => {
+    try {
+        let { lkd_id, date, jde_operator, name_operator, station, login_time } = data
+        const params = [lkd_id, date, jde_operator, name_operator, station, login_time]
+        let result = await db.query(QUERY_STRING.insert_log, params)
+        if(result.rowCount>0){
+            return result.rows[0];
+        }else{
+            return false
+        }
+    } catch (error) {
+        logger.error(error)
+        console.error('Error during update:', error);
+        return false;
+    }
+}
+
+const updateLog = async(data) => {
+    try {
+        let { logout_time, id, date } = data
+        const params = [logout_time, id, date]
+        let result = await db.query(QUERY_STRING.update_log, params)
+        if(result.rowCount>0){
+            return result.rows[0];
+        }else{
+            return false
+        }
+    } catch (error) {
+        logger.error(error)
+        console.error('Error during update:', error);
+        return false;
+    }
+}
+
+module.exports = {
+    insertLog,
+    updateLog
+}
